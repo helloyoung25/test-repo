99c0cced2ddc567a5096fe6724973a8ac1b789c0
hufs-cjh <rflxn2113@hufs.ac.kr>
2022-03-25 14:58:13 +0900

    refactor main function exit code

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
-main()
+// 20220325 Refactored from ChoiJun Hyuk
+
+int main()		// main function should return exit code
 {
 	printf("Hello World!\n");
+
+	return 0;		// exit code 0 means non-error status
 }
